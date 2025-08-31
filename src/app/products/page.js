1f418e1cbb2c8c42fc1184f3e8634b440295f175
mianzainch000 <mianzainch000@gmail.com>
2025-08-31 12:28:04 +0500

    navbar

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,24 +1,46 @@
-"use client"
-import styles from "@/css/Navbar.module.css"
-import React from 'react'
+"use client";
+import styles from "@/css/Navbar.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 const Navbar = () => {
+    const path = usePathname();
     return (
         <>
             <div className={styles.container}>
-                <div className={styles.w}>
+                <div className={styles.navInner}>
                     <div>Logo</div>
                     <div className={styles.btnContainer}>
-                        <button className={styles.btn}>Products</button>
-                        <button className={styles.btn}>Add Product</button>
-                        <button className={styles.btn}>Cart</button>
+                        <Link href="/products">
+                            <button
+                                className={` ${path === "/products" ? styles.active : styles.btn}`}
+                            >
+                                Products
+                            </button>
+                        </Link>
+                        <Link href="/products">
+                            <button
+                                className={` ${path === "/addProduct" ? styles.active : styles.btn}`}
+                            >
+                                Add Product
+                            </button>
+                        </Link>
+                        <Link href="/products">
+                            <button
+                                className={` ${path === "/cart" ? styles.active : styles.btn}`}
+                            >
+                                Cart
+                            </button>
+                        </Link>
+
                         <button className={styles.btn}>Light Mode</button>
                         <button className={styles.logoutBtn}>Logout</button>
                     </div>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
