b7d2fafc4cb2ddbc7497ec2287fab4412ebeb6dc
mianzainch000 <mianzainch000@gmail.com>
2025-08-31 15:25:40 +0500

    a

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,29 @@
 "use client";
 import Link from "next/link";
-import React, { useState } from "react";
 import styles from "@/css/Navbar.module.css";
 import { usePathname } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import { setCookie, getCookie } from "cookies-next";
 
-const Navbar = () => {
+const Navbar = ({ initialTheme, firstName, lastName }) => {
     const path = usePathname();
     const [menuOpen, setMenuOpen] = useState(false);
+    const [theme, setTheme] = useState(initialTheme || "light");
 
     const toggleMenu = () => setMenuOpen(!menuOpen);
+
+    useEffect(() => {
+        const savedTheme = getCookie("theme") || theme;
+        setTheme(savedTheme);
+        document.documentElement.setAttribute("data-theme", savedTheme);
+    }, []);
+
+    const toggleTheme = () => {
+        const newTheme = theme === "light" ? "dark" : "light";
+        setTheme(newTheme);
+        setCookie("theme", newTheme, { maxAge: 60 * 60 * 24 * 365 });
+        document.documentElement.setAttribute("data-theme", newTheme);
+    };
 
     return (
         <>
@@ -52,14 +67,19 @@ const Navbar = () => {
                         >
                             Cart
                         </Link>
-                        <button className={`${styles.btn} ${styles.btnPrimary}`}>
-                            Light Mode
+                        <button
+                            className={`${styles.btn} ${styles.btnPrimary}`}
+                            onClick={toggleTheme}
+                        >
+                            <span>
+                                {theme === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+                            </span>
                         </button>
                         <button className={`${styles.btn} ${styles.btnRed}`}>Logout</button>
                     </div>
                 </div>
             </div>
-            <h2 className={styles.welcomeText}>Welcome Zain Ishfaq</h2>
+            <h2 className={styles.welcomeText}> Welcome {firstName || "Guest"} {lastName || "Guest"}</h2>
         </>
     );
 };
