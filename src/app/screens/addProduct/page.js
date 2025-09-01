714038f3cc1770f227eac8508e4507d6fdd2ff98
mianzainch000 <mianzainch000@gmail.com>
2025-09-01 09:20:50 +0500

    a

--- src/app/screens/addProduct/page.js
+++ src/app/screens/addProduct/page.js
@@ -1,14 +1,47 @@
 "use client";
 import React, { useState } from "react";
 import styles from "@/css/AddProduct.module.css";
+import UploadImageSection from "@/components/UploadImageSection";
 
 const page = () => {
+  //Hooks
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [images, setImages] = useState([]);
   const [sizes, setSizes] = useState([]);
   const [category, setCategory] = useState("");
+
+  // Functions
+
+  const handleSizeChange = (imageIndex, sizeIndex, field, value) => {
+    setSizes((prev) => {
+      const updated = [...prev];
+      updated[imageIndex] = [...(prev[imageIndex] || [])];
+      updated[imageIndex][sizeIndex] = {
+        ...(updated[imageIndex][sizeIndex] || {}),
+        [field]: value,
+      };
+      return updated;
+    });
+  };
+
+  const handleAddSize = (i) => {
+    setSizes((prev) => {
+      const updated = [...prev];
+      updated[i] = [...(prev[i] || []), { name: "", quantity: "" }];
+      return updated;
+    });
+  };
+
+  const handleRemoveSize = (i, j) => {
+    setSizes((prev) => {
+      const updated = [...prev];
+      updated[i] = (prev[i] || []).filter((_, index) => index !== j);
+      return updated;
+    });
+  };
+
   return (
     <>
       <div className={styles.wrapper}>
@@ -47,14 +80,14 @@ const page = () => {
             onChange={(e) => setCategory(e.target.value)}
           />
 
-          {/* <UploadImageSection
+          <UploadImageSection
             images={images}
             onUpdate={setImages}
             sizes={sizes}
             onSizeChange={handleSizeChange}
             onAddSize={handleAddSize}
             onRemoveSize={handleRemoveSize}
-          /> */}
+          />
 
           <button type="submit" className={styles.submitBtn}>
             Add Product
