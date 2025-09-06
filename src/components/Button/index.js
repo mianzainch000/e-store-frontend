6153f0e6ab952e563ca37c912021016017e63927
mianzainch000 <mianzainch000@gmail.com>
2025-09-06 16:16:52 +0500

    custom button

---
+++ src/components/Button/index.js
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import styles from "@/css/Button.module.css";
+
+const CustomButton = ({
+    type = "button",
+    text,
+    onClick,
+    variant = "primary",
+    disabled = false,
+    startIcon,
+    endIcon,
+}) => {
+    return (
+        <button
+            type={type}
+            className={`${styles.btn} ${styles[variant]}`}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {startIcon && <span className={styles.icon}>{startIcon}</span>}
+            {text}
+            {endIcon && <span className={styles.icon}>{endIcon}</span>}
+        </button>
+    );
+};
+
+export default CustomButton;
