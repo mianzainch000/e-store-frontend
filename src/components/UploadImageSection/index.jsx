c80314aeb2a9f1d3bd3c841a5ccce43710bc23f2
mianzainch000 <mianzainch000@gmail.com>
2025-09-07 10:32:42 +0500

    button

--- src/components/UploadImageSection/index.jsx
+++ src/components/UploadImageSection/index.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import styles from "./UploadImageSection.module.css";
+import styles from "@/css/UploadImageSection.module.css";
 
 const UploadImageSection = ({
   images,
